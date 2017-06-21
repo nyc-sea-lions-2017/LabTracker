7ee937e57bd442cbe3a312ebc3934aa582f0cbfc
gus-christensen <augustjc@gmail.com>
2017-06-21 17:21:31 -0400

    index, etc. seeds. corrections to validations.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,5 @@
 class Comment < ApplicationRecord
   belongs_to :commenter, class_name: 'User'
   belongs_to :commentable, polymorphic: true
-  validates :commenter_id, :commentable_type, :commenter_id, :body
+  validates :commenter_id, :commentable_type, :commenter_id, :body, presence: true
 end

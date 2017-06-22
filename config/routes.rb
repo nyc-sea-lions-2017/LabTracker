66214e1972dd36b4c8436aa390da9c286a8dc2ac
gus-christensen <augustjc@gmail.com>
2017-06-22 09:25:07 -0400

    nested experiments in proposals; added 'proposal has_one experiment'; proposal views

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :proposals
+  resources :proposals do
+    resources :experiments
+  end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
